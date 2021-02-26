ee70917c38ca43e7e5e7b9fae104057b3627672f
LiCern <lisacernilogar@me.com>
2021-02-26 20:32:47 +0000

    Create basic ListItem component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import React from 'react';
+import ListItem from './components/ListItem';
 
 function App() {
-  const [people, setPeople] = React.useState({});
+  const [people, setPeople] = React.useState([]);
   fetch('https://jsonplaceholder.typicode.com/users')
     .then((res) => res.json())
     .then((data) => setPeople(data));
-  return <div className="App">{`${people}`}</div>;
+  return (
+    <div className="App">
+      {people.map((person) => {
+        return <ListItem person={person} />;
+      })}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ListItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ListItem = ({ person }) => {
+  return <div>{person.name}</div>;
+};
+
+export default ListItem;
